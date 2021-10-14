ecd9662fb69be628d22562cef74c26136e5b78e9
flamechain <rwc@thezulus.com>
2021-10-13 20:18:57 -0700

    testing

---
+++ src/old/test.py
@@ -0,0 +1,31 @@
+
+def main():
+    print("Hello, world!");
+
+def uselessfunc():
+    a = 1 + 1;
+    a = 1 + a;
+    a = 1 + a;
+    a = 1 + a;
+    a = 1 + a;
+    a = 1 + a;
+    a = 1 + a;
+    a = 1 + a;
+    a = 1 + a;
+    a = 1 + a;
+    a = 1 + a;
+    a = 1 + a;
+    a = 1 + a;
+    a = 1 + a;
+    a = 1 + a;
+    a = 1 + a;
+    a = 1 + a;
+    a = 1 + a;
+    a = 1 + a;
+    a = 1 + a;
+    a = 1 + a;
+    a = 1 + a;
+    a = 1 + a;
+
+if __name__ == "__main__":
+    main()
